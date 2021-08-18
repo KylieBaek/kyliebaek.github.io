8fb35a44814be4dccfda2af6b6020c328731ff61
Seung Yeon Baek <chris8w8@naver.com>
2021-08-18 22:19:16 +0900

    [Update] Apply components

--- src/App.js
+++ src/App.js
@@ -4,13 +4,22 @@ import "./App.css";
 import { Button } from "antd";
 import React from "react";
 
+import Home from "./components/Home";
+import About from "./components/About";
+import Blog from "./components/Blog";
+import Contact from "./components/Contact";
+import Header from "./components/Header";
+import Portfolio from "./components/Portfolio";
+
 function App() {
   return (
     <React.Fragment>
-      <div>My First Resume</div>
-      <Button danger type="primary">
-        test
-      </Button>
+      <Home />
+      <Header />
+      <About />
+      <Blog />
+      <Portfolio />
+      <Contact />
     </React.Fragment>
   );
 }
