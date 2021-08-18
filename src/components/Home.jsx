381ddfa251f5f8a1034cef3e74d8964ac9e6dc83
Seung Yeon Baek <chris8w8@naver.com>
2021-08-18 22:16:51 +0900

    [Component] Create home.jsx

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import Image from "../assets/home-background.jpg";
+
+function Home() {
+  return (
+    <>
+      <img style={{ width: "100%" }} art="background" src={Image} />
+      <span style={{ fontSize: "50px" }}>안녕하세요. 백승연입니다.</span>
+    </>
+  );
+}
+
+export default Home;
